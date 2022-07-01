0deb46f2d4e892af1ec5981e8ed96d0a0fd376a7
rafalzacher1 <rafalzacher99@gmail.com>
2022-07-01 11:25:17 +0100

    Created the tests interface and questions.

---
+++ .history/scripts/tests_20220701013255.js
@@ -0,0 +1,42 @@
+var labels = [
+  "OOP I"
+];
+
+var data = [95, 99, 60, 91, 36, 95, 40, 95, 95];
+
+var ctx = document.getElementById("tests");
+var data = {
+  labels: labels,
+  datasets: [
+    {
+      defaultFontColor: "red",
+      backgroundColor: "rgba(32, 80, 251, .4)",
+      borderColor: "#2050FB",
+      pointBackgroundColor: "#2050FB",
+      pointBorderColor: "#2050FB",
+      lineTension: 0,
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(179,181,198,1)",
+      data: data,
+    },
+  ],
+};
+var myRadarChart = new Chart(ctx, {
+  type: "radar",
+  data: data,
+  options: {
+    legend: { display: false },
+    responsive: true,
+    scale: {
+      reverse: false,
+      pointLabels: {
+        fontColor: "#2050FB",
+        fontSize: 16,
+        fontFamily: "Baloo Bhai",
+      },
+      ticks: {
+        // defaultFontSize: true
+      },
+    },
+  },
+});
---
+++ .history/scripts/tests_20220701013346.js
@@ -0,0 +1,44 @@
+var labels = [
+  "OOP I",
+  "OOP II",
+  "Linked-lists"
+];
+
+var data = [61, ];
+
+var ctx = document.getElementById("tests");
+var data = {
+  labels: labels,
+  datasets: [
+    {
+      defaultFontColor: "red",
+      backgroundColor: "rgba(32, 80, 251, .4)",
+      borderColor: "#2050FB",
+      pointBackgroundColor: "#2050FB",
+      pointBorderColor: "#2050FB",
+      lineTension: 0,
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(179,181,198,1)",
+      data: data,
+    },
+  ],
+};
+var myRadarChart = new Chart(ctx, {
+  type: "radar",
+  data: data,
+  options: {
+    legend: { display: false },
+    responsive: true,
+    scale: {
+      reverse: false,
+      pointLabels: {
+        fontColor: "#2050FB",
+        fontSize: 16,
+        fontFamily: "Baloo Bhai",
+      },
+      ticks: {
+        // defaultFontSize: true
+      },
+    },
+  },
+});
